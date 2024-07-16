bd8243ef7de4a7511efaf86de936ed5f6631709e
David Saah <dasorange.hope@gmail.com>
2024-07-16 21:11:31 +0000

    feat: define simulate route

--- routes.go
+++ routes.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/DaveSaah/risky/controllers"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,6 +12,7 @@ import (
 func initRoutes(e *echo.Echo) {
 	e.GET("/", homeHandler)
 	e.GET("/results", resultsHandler)
+	e.POST("/simulate", simulateHandler)
 }
 
 // homeHandler handlers route to /
@@ -20,4 +23,16 @@ func homeHandler(c echo.Context) error {
 // resultsHandler handles routes to /results
 func resultsHandler(c echo.Context) error {
 	return c.Render(http.StatusOK, "results", nil)
+}
+
+// simulateHandler handles all logic from validation to simulation
+func simulateHandler(c echo.Context) error {
+	tradeInfo, err := controllers.Validate(c)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Trade info: %v\n", tradeInfo)
+
+	return nil
 }
